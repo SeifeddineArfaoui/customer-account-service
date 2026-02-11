a7c8f5cbb795f3eed7b79ccb336f526641fe1e65
Seifeddine Arfaoui <seif.arfaoui06@gmail.com>
2026-02-11 22:21:19 +0100

    initial spring boot project

---
+++ src/test/java/de/seifeddine/customer_account_service/CustomerAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.seifeddine.customer_account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
