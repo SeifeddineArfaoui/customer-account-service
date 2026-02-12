5b4e6089e1570073505e87d48d0173ad65c35207
Seifeddine Arfaoui <seif.arfaoui06@gmail.com>
2026-02-12 11:56:02 +0100

    add customer entity

---
+++ src/main/java/de/seifeddine/customer_account_service/entity/Customer.java
@@ -0,0 +1,28 @@
+package de.seifeddine.customer_account_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
